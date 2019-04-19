8006602c77f11906ce572e9ce000947ebdbb1bc8
Sushmitha DT <dtsushmitha@gmail.com>
2019-04-19 19:57:09 +0530

    Updates

---
+++ precache-manifest.a7ba2e7340daf637b5fd193dd49b07ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "baec8c088cfef7a5175f",
+    "url": "/React-Counter-app/static/js/runtime~main.30c4a898.js"
+  },
+  {
+    "revision": "886525f8a23b00380849",
+    "url": "/React-Counter-app/static/js/main.e2e57676.chunk.js"
+  },
+  {
+    "revision": "123fb41c3fb46a6d1e24",
+    "url": "/React-Counter-app/static/js/2.a5adb38c.chunk.js"
+  },
+  {
+    "revision": "886525f8a23b00380849",
+    "url": "/React-Counter-app/static/css/main.5318ca42.chunk.css"
+  },
+  {
+    "revision": "d1b48bb8def9ed2c0fb670879aaff243",
+    "url": "/React-Counter-app/index.html"
+  }
+];
